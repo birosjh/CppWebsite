5481cc0577d527b0ab07f103e64c334d6c72dfbc
Jacob Biros <jakebiros@gmail.com>
2024-04-02 21:19:26 +0900

    Add basic script using crow

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,15 @@
-#include "oatpp/web/server/HttpConnectionHandler.hpp"
+#include "crow.h"
 
-#include "oatpp/network/Server.hpp"
-#include "oatpp/network/tcp/server/ConnectionProvider.hpp"
+int main()
+{
+    crow::SimpleApp app; //define your crow application
 
-void run() {
+    //define your endpoint at the root directory
+    CROW_ROUTE(app, "/")([](){
+        auto page = crow::mustache::load_text("main.html");
+        return page;
+    });
 
-  /* Create Router for HTTP requests routing */
-  auto router = oatpp::web::server::HttpRouter::createShared();
-
-  /* Create HTTP connection handler with router */
-  auto connectionHandler = oatpp::web::server::HttpConnectionHandler::createShared(router);
-
-  /* Create TCP connection provider */
-  auto connectionProvider = oatpp::network::tcp::server::ConnectionProvider::createShared({"localhost", 8000, oatpp::network::Address::IP_4});
-
-  /* Create server which takes provided TCP connections and passes them to HTTP connection handler */
-  oatpp::network::Server server(connectionProvider, connectionHandler);
-
-  /* Print info about server port */
-  OATPP_LOGI("MyApp", "Server running on port %s", connectionProvider->getProperty("port").getData());
-
-  /* Run server */
-  server.run();
-}
-
-int main() {
-
-  /* Init oatpp Environment */
-  oatpp::base::Environment::init();
-
-  /* Run App */
-  run();
-
-  /* Destroy oatpp Environment */
-  oatpp::base::Environment::destroy();
-
-  return 0;
-
+    //set the port, set the app to run on multiple threads, and run the app
+    app.bindaddr("0.0.0.0").port(8000).multithreaded().run();
 }
